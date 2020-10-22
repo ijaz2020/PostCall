2ca740398a0c89f96cbad7688eaca2097f9b447b
Ijaz Ahamed <ijaz.ahamed@freshworks.com>
2020-10-22 13:34:18 +0530

    DS file is removed and Conversion helper is added
    Removed DS File and added conversion helper

--- src/com/cp/Utils/ConversionUtil.java
+++ src/com/cp/Utils/ConversionUtil.java
@@ -1,2 +1,19 @@
-package com.cp.Utils;public class ConversionUtil {
+package com.cp.Utils;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ConversionUtil {
+    public static void main(String[] args) {
+        List<int[]> res = new ArrayList<>();
+        System.out.println(Arrays.deepToString(res.toArray(new int[res.size()][2])));
+        List<Integer> res1 = new ArrayList<>();
+        System.out.println(Arrays.toString(res1.stream().mapToInt(i -> i).toArray()));
+        String res2 = "4634354622669035435";
+        // this works only for single digits ex(1,2,3,4,5) - Compound digit will fail( eg; 39,42,94) -> it splits like[3,9,4,2,9,4]
+        System.out.println(Arrays.toString(res2.chars().mapToObj(i -> i-48).mapToInt(i->i).toArray()));
+
+    }
 }
